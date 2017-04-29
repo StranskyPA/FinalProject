e60227d7aba853d802b6048127c5324deef0d438
StranskyPA <stranskypa@hendrix.edu>
2017-04-28 20:34:27 -0500

    Some Progress

--- src/SoundTesting/testing.java
+++ src/SoundTesting/testing.java
@@ -16,13 +16,28 @@ import modeselection.SensorFlagger;
 import modeselection.Transitions;
 import modeselection.util.Logger;
 
-public class soundTesting {
+public class testing {
 	public static void main(String[] args) throws IOException {
-		File songOne = new File("/home/lejos/programs/test1.wav");
+		
+		
 		SensorFlagger<Condition> sonarClose = new SensorFlagger<>(new EV3UltrasonicSensor(SensorPort.S2), s -> s.getDistanceMode());
 		sonarClose.add2(Condition.CLEAR, Condition.TOO_CLOSE, f -> f > .3);
 		
-		ArrayBlockingQueue<File> queue = new ArrayBlockingQueue<File>(5);
+		ArrayBlockingQueue<File> queue = new ArrayBlockingQueue<File>(30);
+		
+		new Thread(() -> {
+			while (!Button.ESCAPE.isDown()) {
+				try {
+					File testFile = queue.take();
+					Logger.EV3Log.format("File:%s", testFile.toString());
+					Sound.playSample(testFile, 100);
+					Logger.EV3Log.log("Song over");
+					
+				} catch (InterruptedException e) {
+					Logger.EV3Log.log("No song played");
+		            e.printStackTrace();
+				}
+		}}).start();
 		
 		Transitions<Condition,Mode> transition = new Transitions<>();
 		transition.add(Condition.TOO_CLOSE, Mode.PLAY)
@@ -40,7 +55,7 @@ public class soundTesting {
 				.mode(Mode.PLAY, 
 						transition, 
 						() -> {
-							//File songOne = new File("/home/lejos/programs/dumb.wav");
+							File songOne = new File("/home/lejos/programs/test1.wav");
 							queue.add(songOne);
 							Motor.A.stop(true);
 							Motor.D.stop();
@@ -49,18 +64,7 @@ public class soundTesting {
 		
 		controller.control();
 		
-		new Thread(() -> {
-			while (!Button.ESCAPE.isDown()) {
-				try {
-					File testFile = queue.take();
-					Logger.EV3Log.format("File:%s", testFile.toString());
-					Sound.playSample(testFile, 100);
-					Logger.EV3Log.log("Song over");
-					
-				} catch (InterruptedException e) {
-		            e.printStackTrace();
-				}
-		}}).start();
+		
 		
 		System.exit(0);
 	}
